1b823a5a91095743305f1f5f1fb94eba0380749e
Lindsay Chen <lindsay.chen@yale.edu>
2021-06-06 20:26:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,9 @@ def join_nested_strings(src)
   while count < src.length do
     inner_count = 0 
     while inner_count < src[count].length do
-      
+      if src[count][inner_count].class == Strings
+        new_array << src[count][inner_count]
+      end
       inner_count += 1
     end
     count +=1
